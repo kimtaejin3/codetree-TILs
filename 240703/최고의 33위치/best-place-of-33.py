33931f288d1a1adb3dd478b866feb67d97ed04ec
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-07-03 15:53:18 +0900

    Title: 최고의 33위치, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240703/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+ans = -1
+for i in range(n):
+
+    for j in range(n):
+        cnt = 0
+
+        if j + n > n:
+            continue
+
+        for k in range(i+3):
+            for l in range(j+3):
+                if k > n-1 or l > n-1:
+                    continue
+                cnt += arr[k][l]
+    
+        ans = max(ans, cnt)
+
+
+print(ans)
