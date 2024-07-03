d5e8dff446363e33cf75ae6c17e9e1ec325a5f1b
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-07-03 16:38:32 +0900

    Title: 행복한 수열의 개수, Time: 143ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240703/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,43 @@
+# 배열을 입력으로 받고 연속 수열인지 판단하는 함수
+# 다 완성은 했지만 더 나은 해결책이 있으려나
+def isHappySequence(datas,m):
+    
+    for i in range(len(datas)):
+        target = datas[i]
+        cnt = 0
+        lastPosition = -1 
+        for j in range(len(datas)):
+            if (target == datas[j] and (j-lastPosition == 1 or lastPosition == -1)):
+                cnt += 1
+                lastPosition = j
+        
+        if cnt >= m:
+            return True
+
+    return False
+
+n,m = map(int,input().split())
+
+arr = [list(map(int,input().split())) for _ in range(n)]
+
+# 또 하나의 고민 세로 배열 추출을 어떻게 하면 좋을까
+
+# 일단은 가로 배열들부터 해결해보자
+
+ans = 0
+
+for a in arr:
+    if isHappySequence(a,m):
+        ans += 1
+
+
+# 일단 제일 먼저 생각나는 방법으로 세로 배열들을 구해보자
+for i in range(n):
+    tempArr = []
+    for j in range(n):
+        tempArr.append(arr[j][i])
+    
+    if isHappySequence(tempArr,m):
+        ans += 1
+
+print(ans)
