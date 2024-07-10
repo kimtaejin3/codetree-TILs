3301fd8de9b7e87d223f3222d7b3a7abfca59a35
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-07-10 15:40:37 +0900

    Title: 일렬로 서있는 소 2, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

--- 240710/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240710/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -6,7 +6,7 @@ cnt = 0
 for i in range(n):
     for j in range(n):
         for k in range(n):
-            if i < j < k and arr[i] < arr[j] < arr[k]:
+            if i < j < k and arr[i] <= arr[j] <= arr[k]:
                 cnt += 1
 
 print(cnt)
