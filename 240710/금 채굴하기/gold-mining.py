62245d88341d0eaef2cfb8cbf2d68c3aa7557e37
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-07-10 15:28:52 +0900

    Title: 금 채굴하기, Time: 308ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 240710/금 채굴하기/gold-mining.py
+++ 240710/금 채굴하기/gold-mining.py
@@ -34,6 +34,8 @@ for k in range(n):
     if n % 2 == 0:
         for center_x in range(n//2-2, n//2+2):
             for center_y in range(n//2-2, n//2+2):
+                if center_x < 0 or center_x > n-1 or center_y < 0 or center_y > n-1:
+                    continue
 
                 mining_area = generateMiningArea(k,center_x,center_y)
 
