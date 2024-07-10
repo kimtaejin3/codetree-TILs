9b1906faceace739b5228698701cd118be63cd00
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-07-10 15:32:47 +0900

    Title: 금 채굴하기, Time: 245ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 240710/금 채굴하기/gold-mining.py
+++ 240710/금 채굴하기/gold-mining.py
@@ -13,6 +13,7 @@
 # -> 특정 4개로 center position candidate를 좁협더니 95% 에서 틀렸다
 # -> 그래서 candidate 범위를 딱 1을 늘렸더니 97%에서 틀렸다, 틀린것 보면 하나같이 1이 부족
 # -> 5로 늘리니까 시간초과,, 2로 가자
+# -> 접근은 맞는 거 같은데,, 
 
 n,m = map(int,input().split())
 
@@ -34,8 +35,8 @@ ans = 0
 
 for k in range(n):
     if n % 2 == 0:
-        for center_x in range(n//2-3, n//2+3):
-            for center_y in range(n//2-3, n//2+3):
+        for center_x in range(n//2-4, n//2+4):
+            for center_y in range(n//2-4, n//2+4):
                 if center_x < 0 or center_x > n-1 or center_y < 0 or center_y > n-1:
                     continue
 
