78c9313dea2b3ab10547e55e814f46e2e2e406ec
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-09-03 13:26:58 +0900

    Title: 피보나치 수, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240903/피보나치 수/fibonacci-number.py
+++ 240903/피보나치 수/fibonacci-number.py
@@ -1,8 +1,13 @@
 def fibo(n):
     if n <= 2:
         return 1
+    
+    if memo[n] > 0:
+        return memo[n]
 
-    return fibo(n-1) + fibo(n-2)
+    memo[n] = fibo(n-1) + fibo(n-2)
+    return memo[n]
 
-
-print(fibo(int(input())))
+n = int(input())
+memo = [-1] * (n+1)
+print(fibo(n))
