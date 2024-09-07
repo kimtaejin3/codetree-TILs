42d1895f948b12c09bd40b46d92435828dbf85b0
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-09-07 20:09:41 +0900

    Title: 마법의 숲 탐색, Time: 510ms, Memory: 29MB, Status: Passed - CodeTree

--- 240907/마법의 숲 탐색/magical-forest-exploration.py
+++ 240907/마법의 숲 탐색/magical-forest-exploration.py
@@ -1,76 +1,93 @@
-# 시뮬레이션을 통한 골렘을 좌표(배열)에 표시 
-# 골렘들은 배열에 가지고 있고 위에서부터 아래로 한칸씩 이동 - 이때 검증이 들어감
-# 골렘의 이동이 끝났으면 좌표 업데이트 (출구는 다른 값 넣기 ex. 출구는 2, 나머지는 1 이런식으로)
-# 정령을 남쪽으로 이동시키는 시뮬레이션하기 - 이때도 검증 들어감
+from collections import deque
 
-def adjustGrid(grid,x,y):
-    for i in range(-1,2):
-            if x+i <0 or x+i > R-1 or y-1<0 or y+1>C-1:
-                continue
-            
-            if grid[x+i][y-1] > 0 or grid[x+i][y]>0 or grid[x+i][y+1]>0:
-                return -1
+MAX_L = 70
 
-            grid[x+i][y-1] = golrem[i+1][0]
-            grid[x+i][y] = golrem[i+1][1]
-            grid[x+i][y+1] = golrem[i+1][2]
-    return grid
+R, C, K = 0, 0, 0 # 행, 열, 골렘의 개수를 의미합니다
+A = [[0] * MAX_L for _ in range(MAX_L + 3)] # 실제 숲을 [3~R+2][0~C-1]로 사용하기위해 행은 3만큼의 크기를 더 갖습니다
+dy = [-1, 0, 1, 0]
+dx = [0, 1, 0, -1]
+isExit = [[False] * MAX_L for _ in range(MAX_L + 3)] # 해당 칸이 골렘의 출구인지 저장합니다
+answer = 0 # 각 정령들이 도달할 수 있는 최하단 행의 총합을 저장합니다
 
-R, C, k = map(int,input().split())
+# (y, x)가 숲의 범위 안에 있는지 확인하는 함수입니다
+def inRange(y, x):
+    return 3 <= y < R + 3 and 0 <= x < C
 
-grid = [
-    [0]*C for _ in range(R)
-]
+# 숲에 있는 골렘들이 모두 빠져나갑니다
+def resetMap():
+    for i in range(R + 3):
+        for j in range(C):
+            A[i][j] = 0
+            isExit[i][j] = False
 
-golremsInfo = []
+# 골렘의 중심이 y, x에 위치할 수 있는지 확인합니다.
+# 북쪽에서 남쪽으로 내려와야하므로 중심이 (y, x)에 위치할때의 범위와 (y-1, x)에 위치할떄의 범위 모두 확인합니다
+def canGo(y, x):
+    flag = 0 <= x - 1 and x + 1 < C and y + 1 < R + 3
+    flag = flag and (A[y - 1][x - 1] == 0)
+    flag = flag and (A[y - 1][x] == 0)
+    flag = flag and (A[y - 1][x + 1] == 0)
+    flag = flag and (A[y][x - 1] == 0)
+    flag = flag and (A[y][x] == 0)
+    flag = flag and (A[y][x + 1] == 0)
+    flag = flag and (A[y + 1][x] == 0)
+    return flag
 
-for _ in range(k):
-    golremsInfo.append(tuple(list(map(int,input().split()))))
+# 정령이 움직일 수 있는 모든 범위를 확인하고 도달할 수 있는 최하단 행을 반환합니다
+def bfs(y, x):
+    result = y
+    q = deque([(y, x)])
+    visit = [[False] * C for _ in range(R + 3)]
+    visit[y][x] = True
+    while q:
+        cur_y, cur_x = q.popleft()
+        for k in range(4):
+            ny, nx = cur_y + dy[k], cur_x + dx[k]
+            # 정령의 움직임은 골렘 내부이거나
+            # 골렘의 탈출구에 위치하고 있다면 다른 골렘으로 옮겨 갈 수 있습니다
+            if inRange(ny, nx) and not visit[ny][nx] and (A[ny][nx] == A[cur_y][cur_x] or (A[ny][nx] != 0 and isExit[cur_y][cur_x])):
+                q.append((ny, nx))
+                visit[ny][nx] = True
+                result = max(result, ny)
+    return result
 
-for i in range(k):
-    c, d = golremsInfo[i]
-
-    # 2는 골렘, 3은 출구
-    golrem = [[0,1,0],[1,2,1],[0,1,0]]
-
-    if d == 0:
-        golrem[0][1] = 3
-    elif d == 1:
-        golrem[1][2] = 3
-    elif d == 2:
-        golrem[2][1] = 3
-    elif d == 3:
-        golrem[1][0] = 3
-    
-    x, y = 0, c - 1
-    isLeft = True
-
-    while True:
-        nx = x + 1
-
-
-        newGrid = adjustGrid(grid,nx,y)
-
-        # 땅에 닿았거나 더 이상 이동할 수 없을 때
-        if nx > R - 2:
-            # grid = newGrid
-
-            # for g in grid:
-                # print(g)
-
-            print('=== end ===')
-            break
-        
-        if newGrid == -1:
-            print("!!")
-            if isLeft:
-                y = y - 1
-            else:
-                y = y + 1
-            if y < 0:
-                y = 0
-                isLeft = False
+# 골렘id가 중심 (y, x), 출구의 방향이 d일때 규칙에 따라 움직임을 취하는 함수입니다
+# 1. 남쪽으로 한 칸 내려갑니다.
+# 2. (1)의 방법으로 이동할 수 없으면 서쪽 방향으로 회전하면서 내려갑니다.
+# 3. (1)과 (2)의 방법으로 이동할 수 없으면 동쪽 방향으로 회전하면서 내려갑니다.
+def down(y, x, d, id):
+    if canGo(y + 1, x):
+        # 아래로 내려갈 수 있는 경우입니다
+        down(y + 1, x, d, id)
+    elif canGo(y + 1, x - 1):
+        # 왼쪽 아래로 내려갈 수 있는 경우입니다
+        down(y + 1, x - 1, (d + 3) % 4, id)
+    elif canGo(y + 1, x + 1):
+        # 오른쪽 아래로 내려갈 수 있는 경우입니다
+        down(y + 1, x + 1, (d + 1) % 4, id)
+    else:
+        # 1, 2, 3의 움직임을 모두 취할 수 없을떄 입니다.
+        if not inRange(y-1, x-1) or not inRange(y+1, x+1):
+            # 숲을 벗어나는 경우 모든 골렘이 숲을 빠져나갑니다
+            resetMap()
         else:
-            x = nx
-        
-        print('move: ', x, y)
+            # 골렘이 숲 안에 정착합니다
+            A[y][x] = id
+            for k in range(4):
+                A[y + dy[k]][x + dx[k]] = id
+            # 골렘의 출구를 기록하고
+            isExit[y + dy[d]][x + dx[d]] = True
+            global answer
+            # bfs를 통해 정령이 최대로 내려갈 수 있는 행를 계산하여 누적합니다
+            answer += bfs(y, x) - 3 + 1
+
+def main():
+    global R, C, K
+    R, C, K = map(int, input().split())
+    for id in range(1, K + 1): # 골렘 번호 id
+        x, d = map(int, input().split()) # 골렘의 출발 x좌표, 방향 d를 입력받습니다
+        down(0, x - 1, d, id)
+    print(answer)
+
+if __name__ == "__main__":
+    main()
