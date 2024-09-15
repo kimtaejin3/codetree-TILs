5415c2d4cdc978dca46b10dda6e4a2101691f267
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-09-15 18:08:27 +0900

    Title: 기울어진 직사각형의 회전, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

--- 240915/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
+++ 240915/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -1,49 +1,45 @@
+CCW = 0
+CW = 1
+
 n = int(input())
 
-grid = [list(map(int,input().split())) for _ in range(n)]
-r,c,m1,m2,m3,m4,d = map(int,input().split())
+grid = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
 
-rectangle = []
+temp = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
 
-dxs = [-1,-1,1,1]
-dys = [1,-1,-1,1]
+def shift(x, y, k, l, move_dir):
+    if move_dir == CCW:
+        dxs, dys = [-1, -1, 1, 1], [1, -1, -1, 1]
+        move_nums = [k, l, k, l]
+    else:
+        dxs, dys = [-1, -1, 1, 1], [-1, 1, 1, -1]
+        move_nums = [l, k, l, k]
+    
+    for i in range(n):
+        for j in range(n):
+            temp[i][j] = grid[i][j]
+    
+    for dx, dy, move_num in zip(dxs, dys, move_nums):
+        for _ in range(move_num):
+            nx, ny = x + dx, y + dy
+            temp[nx][ny] = grid[x][y]
+            x,y = nx, ny
+    
+    for i in range(n):
+        for j in range(n):
+            grid[i][j] = temp[i][j]
 
-tries_count = [m1, m2, m3, m4]
-tr,tc = r-1, c-1
-rectangle.append(grid[tr][tc])
 
-def inRange(r,c):
-    return 0 <= r < n and 0 <= c < n 
+x, y, m1, m2, m3, m4, d = tuple(map(int,input().split()))
+shift(x - 1, y - 1, m1, m2, d)
 
-for i in range(len(tries_count)):
-    count = tries_count[i]
-    for j in range(count):
-        tr, tc = tr + dxs[i], tc + dys[i]
-        if inRange(tr,tc):
-            rectangle.append(grid[tr][tc])
-
-rectangle.pop()
-
-if d == 0:
-    temp = rectangle.pop()
-    rectangle.insert(0,temp)
-elif d == 1:
-    temp = rectangle.pop(0)
-    rectangle.append(temp)
-
-tr, tc = r - 1, c - 1
-grid[tr][tc] = rectangle[0]
-index = 1
-
-for i in range(len(tries_count)):
-    count = tries_count[i]
-    for j in range(count):
-        tr, tc = tr + dxs[i], tc + dys[i]
-        if inRange(tr,tc) and index < len(rectangle):
-            grid[tr][tc] = rectangle[index]
-            index += 1
-
-for row in grid:
-    for elem in row:
-        print(elem, end=' ')
+for i in range(n):
+    for j in range(n):
+        print(grid[i][j], end=" ")
     print()
