30f27e9a333c4fce4b2baa7306c4c9070377ddcd
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-09-15 17:36:22 +0900

    Title: 기울어진 직사각형의 회전, Time: 153ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240915/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -0,0 +1,49 @@
+n = int(input())
+
+grid = [list(map(int,input().split())) for _ in range(n)]
+r,c,m1,m2,m3,m4,d = map(int,input().split())
+
+rectangle = []
+
+dxs = [-1,-1,1,1]
+dys = [1,-1,-1,1]
+
+tries_count = [m1, m2, m3, m4]
+tr,tc = r-1, c-1
+rectangle.append(grid[tr][tc])
+
+def inRange(r,c):
+    return 0 <= r < n - 1 and 0 <= c < n - 1
+
+for i in range(len(tries_count)):
+    count = tries_count[i]
+    for j in range(count):
+        tr, tc = tr + dxs[i], tc + dys[i]
+        if inRange(tr,tc):
+            rectangle.append(grid[tr][tc])
+
+rectangle.pop()
+
+if d == 0:
+    temp = rectangle.pop()
+    rectangle.insert(0,temp)
+elif d == 1:
+    temp = rectangle.pop(0)
+    rectangle.append(temp)
+
+tr, tc = r - 1, c - 1
+grid[tr][tc] = rectangle[0]
+index = 1
+
+for i in range(len(tries_count)):
+    count = tries_count[i]
+    for j in range(count):
+        tr, tc = tr + dxs[i], tc + dys[i]
+        if inRange(tr,tc) and index < len(rectangle):
+            grid[tr][tc] = rectangle[index]
+            index += 1
+
+for row in grid:
+    for elem in row:
+        print(elem, end=' ')
+    print()
