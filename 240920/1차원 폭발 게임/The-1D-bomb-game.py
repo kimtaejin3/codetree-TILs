9e38a6a7eb86480a85185ca4bdf831e4ccfdb413
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-09-20 10:18:01 +0900

    Title: 1차원 폭발 게임, Time: 5099ms, Memory: 26MB, Status: Time Limit Exceed - CodeTree

---
+++ 240920/1차원 폭발 게임/The-1D-bomb-game.py
@@ -0,0 +1,45 @@
+n, m = map(int, input().split())
+
+arr = []
+for _ in range(n):
+    val = int(input())
+    arr.append(val)
+
+# 포인터 2개쓰기
+
+def find():
+    global arr
+    start, end = 0, 1
+    
+    while end < len(arr):
+        if arr[start] == arr[end]:
+            end += 1
+        else:
+            if end - start >= m:
+                for i in range(start, end):
+                    arr[i] = -1
+            start = end
+    if end - start >= m:
+        for i in range(start, end):
+            arr[i] = -1
+
+    while arr.count(-1) > 0:
+        arr.remove(-1)
+
+def isExist():
+    global arr
+    for i in range(len(arr)-1):
+        if arr[i] == arr[i+1]:
+            return True
+    
+    return False
+
+# while isExist():
+#     find()
+
+while isExist():
+    find()
+
+print(len(arr))
+for elem in arr:
+    print(elem)
