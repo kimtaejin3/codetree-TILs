7747ecb763ca0de1903a54cb5ee7d311dc0065df
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-09-21 21:31:37 +0900

    Title: 단 한 번의 2048 시도, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240921/단 한 번의 2048 시도/one-trial-of-2048-game.py
+++ 240921/단 한 번의 2048 시도/one-trial-of-2048-game.py
@@ -12,7 +12,7 @@ if dir == "R" or dir == "L":
             if row[j] == row[j-1] and row[j] != 0:
                 row[j] += row[j-1]
                 row[j-1] = 0
-        # print(row)
+
         if row.count(0) > 0:
             row.remove(0)
             
@@ -24,7 +24,30 @@ if dir == "R" or dir == "L":
         grid[i] = row
 
 elif dir == "U" or dir == "D":
-    print(1)
+    for i in range(4):
+        col = []
+        for j in range(4):
+            col.append(grid[j][i])
+
+        for j in range(3,0,-1):
+            if col[j] == col[j-1] and col[j] != 0:
+                col[j] += col[j-1]
+                col[j-1] = 0
+
+        while col.count(0) > 0:
+            col.remove(0)
+            
+            if dir == "D":
+                col.insert(0, -1)
+            else:
+                col.append(-1)
+        
+        for j in range(4):
+            if col[j] == -1:
+                col[j] = 0
+
+        for j in range(4):
+            grid[j][i] = col[j] 
 
 for row in grid:
     for elem in row:
