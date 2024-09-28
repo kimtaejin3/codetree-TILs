c0301b847b1686bbfbad22acfe5225481426b5ed
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-09-28 13:34:25 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 112ms, Memory: 25MB, Status: Passed - CodeTree

--- 240928/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
+++ 240928/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -1,48 +1,39 @@
-n, r, c = map(int,input().split())
+n, curr_x, curr_y = tuple(map(int,input().split()))
+a = [[0] * (n+1)]
+for _ in range(n):
+    a.append([0] + list(map(int,input().split())))
 
-grid = [
-    list(map(int,input().split()))
-    for _ in range(n)
-]
+visited_nums = []
 
-visited = [
-    [False for _ in range(n)]
-    for _ in range(n)
-]
+def in_range(x,y):
+    return 1 <= x and x <= n and 1 <= y and y <= n
 
-def inRange(x,y):
-    return 0 <= x <= n-1 and 0 <= y <= n-1
+def can_go(x,y,curr_num):
+    return in_range(x,y) and a[x][y] > curr_num
 
-dxs = [-1,1,0,0]
-dys = [0,0,-1,1]
+def simulate():
+    global curr_x, curr_y
 
-ans = []
+    dxs, dys = [-1,1,0,0],[0,0,-1,1]
 
-def simulate(x,y):
-    global ans
+    for dx, dy in zip(dxs, dys):
+        next_x, next_y = curr_x + dx, curr_y + dy
 
-    cur_x, cur_y = x, y
-    visited[x][y] = True
-    ans.append(grid[x][y])
+        if can_go(next_x,next_y,a[curr_x][curr_y]):
+            curr_x, curr_y = next_x, next_y
+            return True
+    
+    return False
 
-    while True:
-        max_pos = (0,0)
-        max_val = grid[cur_x][cur_y]
+visited_nums.append(a[curr_x][curr_y])
 
-        for dx, dy in zip(dxs,dys):
-            next_x, next_y = cur_x + dx, cur_y + dy
+while True:
+    greater_number_exist = simulate()
 
-            if inRange(next_x, next_y) and not visited[next_x][next_y] and grid[next_x][next_y] > max_val:
-                max_val = grid[next_x][next_y]
-                visited[next_x][next_y] = True
-                max_pos = next_x,next_y
-                break
-        
-        if max_val == grid[cur_x][cur_y]:
-            break
-        ans.append(grid[max_pos[0]][max_pos[1]])
-        cur_x, cur_y = max_pos
+    if not greater_number_exist:
+        break
+    
+    visited_nums.append(a[curr_x][curr_y])
 
-
-simulate(r-1,c-1)
-print(' '.join(map(str, ans)))
+for num in visited_nums:
+    print(num, end=' ')
