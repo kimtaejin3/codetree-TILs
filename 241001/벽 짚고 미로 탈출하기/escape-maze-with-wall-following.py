0540bc8f16223ffd44b7528d60f9c2872c448e8e
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-10-01 17:21:32 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 5023ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

---
+++ 241001/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -0,0 +1,108 @@
+n = int(input())
+x, y = map(int, input().split())
+
+grid = [
+    list(input())
+    for _ in range(n)
+]
+
+dxs = [0,-1,0,1]
+dys = [1,0,-1,0]
+
+d = 0
+
+visited = [
+    [[False,0] for _ in range(n)]
+    for _ in range(n)
+]
+
+def can_go(d):
+    ans = False
+    if d == 0:
+        if x+1>n-1:
+            return False
+        return grid[x+1][y] == "#"
+    elif d == 1:
+        if y+1 > n-1:
+            return False
+        return grid[x][y+1] == "#"
+    elif d == 2:
+        if x-1 < 0:
+            return False
+        return grid[x-1][y] == "#"
+    elif d == 3:
+        if y-1<0:
+            return False
+        return grid[x][y-1] == "#"
+
+    return ans
+
+def is_collision(d):
+    ans = False
+    if d == 0:
+        if y + 1 > n-1:
+            return False
+        return grid[x][y+1] == "#"
+    elif d == 1:
+        if x -1 < 0:
+            return False
+        return grid[x-1][y] == "#"
+    elif d == 2:
+        if y-1 < 0:
+            return False
+        return grid[x][y-1] == "#"
+    elif d == 3:
+        if x+1 > n-1:
+            return False
+        return grid[x+1][y] == "#"
+
+    return ans
+
+t = 0
+
+x -= 1
+y -= 1
+
+visited[x][y][0] = True
+visited[x][y][1] = 0
+
+while True:
+    t += 1
+
+    if can_go(d):
+
+        while is_collision(d):
+            d = (d+1)%4
+
+        x = x + dxs[d]
+        y = y + dys[d]
+        
+        if x < 0 or x > n - 1 or y < 0 or y > n - 1:
+            break
+
+        if visited[x][y][0] and visited[x][y][1] == d:
+            print(-1)
+            exit(0)
+        
+
+        visited[x][y][0] = True
+        visited[x][y][1] = d
+
+    else:
+        
+        d -= 1
+        if d < 0:
+            d = 3
+        
+        x = x + dxs[d]
+        y = y + dys[d]
+
+        if visited[x][y][0] and visited[x][y][1] == d:
+            print(-1)
+            exit(0)
+
+        visited[x][y][0] = True
+        visited[x][y][1] = d
+    
+
+print(t)
