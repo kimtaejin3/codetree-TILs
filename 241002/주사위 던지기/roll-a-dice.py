31b5b604cccd73c9068529e0a467e64509b57ddc
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-10-02 15:13:07 +0900

    Title: 주사위 던지기, Time: 152ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241002/주사위 던지기/roll-a-dice.py
@@ -0,0 +1,60 @@
+n, m, r, c = map(int,input().split())
+orders = input().split()
+
+a = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+# 위(d[0]), 앞(d[1]), 오(d[2])
+dice = [1,2,3]
+
+r -= 1
+c -= 1
+
+def opposite_num(num):
+    return 7 - num
+
+a[r][c] = opposite_num(dice[0])
+
+for i in range(m):
+    order = orders[i]
+    up = dice[0]
+    front = dice[1]
+    right = dice[2]
+
+    nr, nc = r, c
+
+    if order == "D":
+        dice[0] = opposite_num(front)
+        dice[1] = up
+        nr += 1
+        
+    elif order == "U":
+        dice[0] = front
+        dice[1] = opposite_num(up)
+        nr -= 1
+
+    elif order == "L":
+        dice[0] = right
+        dice[2] = opposite_num(up)
+        nc -= 1
+
+    elif order == "R":
+        dice[0] = opposite_num(right)
+        dice[2] = up
+        nc += 1
+    
+    if nr < 0 or nr > n - 1 or nc < 0 or nc > n - 1:
+        continue
+    
+    
+    r, c = nr, nc
+    a[r][c] = opposite_num(dice[0])
+
+ans = 0
+for row in a:
+    for elem in row:
+        ans += elem
+
+print(ans)
