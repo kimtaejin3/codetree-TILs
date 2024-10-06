f49a79fac20e74121efb506516b43840b5f7df0a
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-10-06 19:03:40 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 203ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241006/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -0,0 +1,69 @@
+n, m, t = tuple(map(int, input().split()))
+
+grid = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+
+count = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+next_count = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+dxs, dys = [-1,1,0,0], [0,0,-1,1]
+
+for _ in range(m):
+    r, c = tuple(map(int,input().split()))
+    count[r-1][c-1] = 1
+
+def get_next_pos(r, c):
+    
+    next_pos = [-1,-1]
+    max_val = grid[r][c]
+
+    for dx, dy in zip(dxs, dys):
+        nr, nc = r + dx, c + dy
+        if nr < 0 or nr > n - 1 or nc < 0 or nc > n - 1:
+            continue
+        if grid[nr][nc] > max_val:
+            max_val = grid[nr][nc]
+            next_pos[0], next_pos[1] = nr, nc
+    
+    return (next_pos[0], next_pos[1])
+
+
+def move_all():
+    for i in range(n):
+        for j in range(n):
+            next_count[i][j] = 0
+    
+    for i in range(n):
+        for j in range(n):
+            if count[i][j]:
+                nr, nc = get_next_pos(i,j)
+
+                next_count[nr][nc] += 1
+
+                if next_count[nr][nc] > 1:
+                    next_count[nr][nc] = 0
+    
+    for i in range(n):
+        for j in range(n):
+            count[i][j] = next_count[i][j]
+
+for _ in range(t):
+    move_all()
+
+ans = 0
+
+for i in range(n):
+    for j in range(n):
+        if count[i][j]:
+            ans += 1
+
+print(ans)
