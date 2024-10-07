ffca7f7c5d5636d598656c356c93a1c56f2d8da6
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-10-07 18:05:13 +0900

    Title: 벽이 있는 충돌 실험, Time: 1548ms, Memory: 30MB, Status: Passed - CodeTree

--- 241007/벽이 있는 충돌 실험/collision-experiment-with-wall.py
+++ 241007/벽이 있는 충돌 실험/collision-experiment-with-wall.py
@@ -1,47 +1,31 @@
 T = int(input())
 
-# 0(up), 1(down), 2(left), 3(right)
-dxs, dys = [-1,1,0,0],[0,0,-1,1]
-
 def in_range(x,y):
     return 0 <= x <= n - 1 and 0 <= y <= n - 1
 
-def convert(d):
-    if d == "U":
-        return 0
-    elif d == "D":
-        return 1
-    elif d == "L":
-        return 2
-    else:
-        return 3
-
-def oppose_d(d):
-    if d == "U":
-        return "D"
-    elif d == "D":
-        return "U"
-    elif d == "L":
-        return "R"
-    elif d == "R":
-        return "L"
+mapper = {
+    'U':0,
+    'R':1,
+    'L':2,
+    'D':3
+}
 
 def move(x, y, d):
+    dxs, dys = [-1, 0, 0, 1], [0, 1, -1, 0]
     global next_grid
 
-    next_x, next_y = x + dxs[convert(d)], y + dys[convert(d)]
+    next_x, next_y = x + dxs[d], y + dys[d]
 
     if in_range(next_x, next_y):
         next_grid[next_x][next_y] = (next_grid[next_x][next_y][0] + 1, d)
     else:
-        nd = oppose_d(d)
-        next_grid[x][y] = (next_grid[x][y][0] + 1, nd)
+        next_grid[x][y] = (next_grid[x][y][0] + 1, 3 - d)
 
 def move_all():
     global grid, next_grid
 
     next_grid = [
-        [(0, 'U') for _ in range(n)]
+        [(0, 0) for _ in range(n)]
         for _ in range(n)
     ]
 
@@ -53,28 +37,22 @@ def move_all():
     for i in range(n):
         for j in range(n):
             if next_grid[i][j][0] > 1:
-                next_grid[i][j] = (0,"U")
+                next_grid[i][j] = (0, 0)
 
     for i in range(n):
         for j in range(n):
             grid[i][j] = next_grid[i][j]
 
-# def print_grid():
-    # for i in range(n):
-        # for j in range(n):
-            # print(grid[i][j], end=' ')
-        # print()
-
 for _ in range(T):
     n, m = map(int,input().split())
 
     grid = [
-        [(0, 'U') for _ in range(n)]
+        [(0, 0) for _ in range(n)]
         for _ in range(n)
     ]
 
     next_grid = [
-        [(0, 'U') for _ in range(n)]
+        [(0, 0) for _ in range(n)]
         for _ in range(n)
     ]
     
@@ -82,16 +60,10 @@ for _ in range(T):
         x, y, d = input().split()
         x, y = int(x) - 1, int(y) - 1
 
-        grid[x][y] = (1,d)
+        grid[x][y] = (1, mapper[d])
     
-    # print_grid()
-    # print('== == == == ==')
-
     for __ in range(n*2):
         move_all()
-        # print_grid()
-        # print('== == == == ==')
-
 
     ans = 0
 
