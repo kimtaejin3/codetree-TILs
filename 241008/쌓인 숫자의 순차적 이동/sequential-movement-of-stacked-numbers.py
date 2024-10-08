f51591e12060c5a3a4d390f0fd1a651d6d077ea5
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-10-08 14:33:56 +0900

    Title: 쌓인 숫자의 순차적 이동, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241008/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.py
@@ -0,0 +1,73 @@
+n, m = map(int, input().split())
+
+a = [
+    [list() for _ in range(n)]
+    for _ in range(n)
+]
+
+move_arr = []
+
+for i in range(n):
+    row = list(map(int, input().split()))
+
+    for j in range(n):
+        a[i][j].append(row[j])
+
+nums = list(map(int,input().split()))
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def find_pos(num):
+    for i in range(n):
+        for j in range(n):
+            if num in a[i][j]:
+                return (i, j)
+
+def find_max_pos(x, y):
+    dxs, dys = [-1, -1, -1, 0, 1, 1, 1, 0],[-1, 0, 1, 1, 1, 0, -1, -1]
+
+    max_val, max_pos = -1, (-1, -1)
+
+    for dx, dy in zip(dxs, dys):
+        next_x, next_y = x + dx, y + dy
+
+        #                               리스트 앞은 위쪽임
+        if in_range(next_x, next_y) and len(a[next_x][next_y]) > 0 and max(a[next_x][next_y]) > max_val:
+            max_val = a[next_x][next_y][0]
+            max_pos = (next_x, next_y)
+    
+    return max_pos
+
+def swap(pos1, pos2, num):
+    move_arr = []
+
+    x, y = pos1
+    tx, ty = pos2
+
+    popped = a[x][y].pop(0) 
+    move_arr.append(popped)
+
+    while popped != num:
+        popped = a[x][y].pop(0)
+        move_arr.append(popped)
+    
+    for i in range(len(move_arr)-1,-1,-1):
+        a[tx][ty].insert(0,move_arr[i])
+
+
+def simulate():
+    for num in nums:
+        x, y = find_pos(num)
+        tx, ty = find_max_pos(x, y)
+        swap((x,y), (tx,ty), num)
+    
+
+simulate()
+
+for i in range(n):
+    for j in range(n):
+        if len(a[i][j]) == 0:
+            print(None)
+        else:
+            print(' '.join(map(str,a[i][j])))
