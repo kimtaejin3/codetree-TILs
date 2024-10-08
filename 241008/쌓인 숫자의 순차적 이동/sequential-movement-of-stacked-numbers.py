dc6405dcdd7f205d7c4e89c474e068729bf6856b
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-10-08 15:21:47 +0900

    Title: 쌓인 숫자의 순차적 이동, Time: 154ms, Memory: 25MB, Status: Passed - CodeTree

--- 241008/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.py
+++ 241008/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.py
@@ -40,9 +40,9 @@ def find_max_pos(x, y):
 
         #                               리스트 앞은 위쪽임
         if in_range(next_x, next_y) and len(a[next_x][next_y]) > 0 and max(a[next_x][next_y]) > max_val:
-            max_val = a[next_x][next_y][0]
+            max_val = max(a[next_x][next_y])
             max_pos = (next_x, next_y)
-    
+
     return max_pos
 
 def swap(pos1, pos2, num):
@@ -57,7 +57,7 @@ def swap(pos1, pos2, num):
     while popped != num:
         popped = a[x][y].pop(0)
         move_arr.append(popped)
-    
+
     for i in range(len(move_arr)-1,-1,-1):
         a[tx][ty].insert(0,move_arr[i])
 
@@ -67,8 +67,12 @@ def simulate():
         x, y = find_pos(num)
         tx, ty = find_max_pos(x, y)
         if tx == -1 and ty == -1:
-            break
+            continue
+          
         swap((x,y), (tx,ty), num)
+
+        # print('== == =('+ str(num) +')= == ==')
+        # print_a()
 
 simulate()
 
