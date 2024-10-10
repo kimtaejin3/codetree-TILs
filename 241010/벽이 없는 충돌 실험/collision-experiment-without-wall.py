2a9cc8249ea9a3375df50a3586b8aac07cf1e1cf
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-10-10 15:37:07 +0900

    Title: 벽이 없는 충돌 실험, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241010/벽이 없는 충돌 실험/collision-experiment-without-wall.py
+++ 241010/벽이 없는 충돌 실험/collision-experiment-without-wall.py
@@ -1,105 +1,104 @@
-OFFSET = 1000
+OFFSET = 5
 
-
+N = OFFSET * 2
 T = int(input())
 
 direction_mapper = {
-    "U": 0,
+    "U": 3,
     "L": 1,
     "R": 2,
-    "D": 3,
+    "D": 0,
 }
 
 def in_range(x, y):
-    return 0 <= x <= 2000 and 0 <= y <= 2000
+    return 0 <= x <= N and 0 <= y <= N
 
-def move(x, y, w, n, d):
-    global next_a, a
-    is_collide = False
+def move_all():
+    global marbles
 
     dxs, dys = [-1, 0, 0, 1], [0, -1, 1, 0]
 
-    next_x, next_y = x + dxs[d], y + dys[d]
+    is_collide = False
 
-    if in_range(next_x, next_y):
-        # 중간에 충돌하는 경우 처리
-        if (3-d) in a[next_x][next_y][2]:
-            is_collide = True
-            if w > a[next_x][next_y][0][0]:
-                next_a[next_x][next_y].append((w, n, d))
-            # n이 같을일은 없음.
-            elif w == a[next_x][next_y][0][0] and n > a[next_x][next_y][1]:
-                next_a[next_x][next_y].append((w, n, d))
-        else:
-            next_a[next_x][next_y].append((w, n, d))
+    new_marbles = []
+    for marble in marbles:
+        x, y, w, n, d = marble
+
+        next_x, next_y = x + dxs[d], y + dys[d]
+
+        if in_range(next_x, next_y):
+            flag = False
+
+            for i in range(len(marbles)):
+                tx,ty,tw,tn,td = marbles[i]
+
+                if tx == next_x and ty == next_y and td == 3 - d:
+                    if w < tw:
+                        flag = True
+                    elif w == tw and n < tn:
+                        flag = True
+            
+            if not flag:
+                # print('asdf')
+                new_marbles.append((next_x, next_y, w, n, d))
     
-    return is_collide
+    marbles = sorted(new_marbles, key = lambda x:(x[2], x[3]))
 
 def detect_collision():
+    global grid
     is_collide = False
 
-    for x in range(OFFSET * 2 + 1):
-        for y in range(OFFSET * 2 + 1):
-            if len(next_a[x][y]) > 1:
-                next_a[x][y].sort(key = lambda x:(x[0],x[1]))
-                popped = next_a[x][y][-1]
-                next_a[x][y] = [popped]
+    for x in range(N+1):
+        for y in range(N+1):
+            if grid[x][y] > 1:
                 is_collide = True
-    
-    return is_collide
+                for _ in range(grid[x][y]-1):
+                    for marble in marbles:
+                        tx, ty, tw, tn, td = marble
 
-
-def move_all():
-    global a
-    is_collide = False
-
-    for x in range(OFFSET * 2 + 1):
-        for y in range(OFFSET * 2 + 1):
-            for w, n, d in a[x][y]:
-
-                flag = move(x, y, w, n, d)
-                if flag:
-                    is_collide = True
+                        if tx == x and ty == y:
+                            marbles.remove(marble)
+                            break
     
     return is_collide
 
 
 for _ in range(T):
-    a = [
-        [[] for _ in range(OFFSET * 2 + 1)]
-        for _ in range(OFFSET * 2 + 1)
+    grid = [
+        [0 for _ in range(N + 1)]
+        for _ in range(N + 1)
     ]
 
-    next_a = [
-        [[] for _ in range(OFFSET * 2 + 1)]
-        for _ in range(OFFSET * 2 + 1)
-    ]
+    n = int(input())
+    marbles = []
 
-    N = int(input())
-
-    for i in range(N):
+    for i in range(n):
         x, y, w, d = input().split()
-        a[int(x) + OFFSET][int(y) + OFFSET].append((w, i+1, direction_mapper[d])) 
+        marbles.append((int(y) + OFFSET, int(x) + OFFSET, int(w), i+1, direction_mapper[d]))
     
     ans = -1
 
-    for i in range(OFFSET * 2 + 1, 2):
-        next_a = [
-            [[] for _ in range(OFFSET * 2 + 1)]
-            for _ in range(OFFSET * 2 + 1)
-        ]
-
+    for i in range(0, OFFSET * 2 + 1, 2):
+        for x in range(N+1):
+            for y in range(N+1):
+                grid[x][y] = 0
+        
+        # print('==')
+        # print(marbles)
         flag1 = move_all()
-        flag2 = detect_collision()
+        # print(marbles)
+        # print('==')
 
+        for marble in marbles:
+            x, y, w, n, d = marble
+
+            grid[y][x] += 1
+
+        flag2 = detect_collision()
         if flag1:
             ans = i + 2 - 1
         
         if flag2:
             ans = i + 2
 
-        for x in range(OFFSET * 2 + 1):
-            for y in range(OFFSET * 2 + 1):
-                a[x][y] = next_a[x][y]
-
-print(ans)
+    print(ans)
