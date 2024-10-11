97e4a21f409f49e503943852e7beeb9b21dac0a3
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-10-11 13:28:07 +0900

    Title: 벽이 없는 충돌 실험, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241011/벽이 없는 충돌 실험/collision-experiment-without-wall.py
@@ -0,0 +1,76 @@
+OFFSET = 1000
+
+N = OFFSET * 2
+T = int(input())
+
+direction_mapper = {
+    "U": 3,
+    "L": 1,
+    "R": 2,
+    "D": 0,
+}
+
+def in_range(x, y):
+    return 0 <= x <= N and 0 <= y <= N
+
+def move_all():
+    global marbles
+
+    dxs, dys = [-1, 0, 0, 1], [0, -1, 1, 0]
+
+    is_collide = False
+
+    new_marbles = []
+
+    for marble in marbles:
+        x, y, w, n, d = marble
+
+        next_x, next_y = x + dxs[d], y + dys[d]
+
+        if in_range(next_x, next_y):
+            new_marbles.append((next_x, next_y, w, n, d))
+    
+    marbles = new_marbles
+
+for _ in range(T):
+    grid = {}
+
+    n = int(input())
+    marbles = []
+
+    for i in range(n):
+        x, y, w, d = input().split()
+        marbles.append((int(y) + OFFSET, int(x) + OFFSET, int(w), i+1, direction_mapper[d]))
+    
+    ans = -1
+
+    for i in range(0, N + 1, 2):
+        
+        flag1 = move_all()
+        flag2 = False
+
+        for marble in marbles:
+            x, y, w, n, d = marble
+
+            if not (x,y) in grid:
+                grid[(x,y)] = (w, n, d)
+            else:
+                flag2 = True
+                tw, tn, td = grid[(x,y)]
+                if w > tw or (w == tw and n > tn):
+                    grid(x,y) = (w, n, d)
+                    marbles.remove((x, y, tw, tn, td))
+                else:
+                    gird(x,y) = (tw, tn, td)
+                    marbles.remove((x, t, w, n, d))
+
+            grid[y][x] += 1
+
+        
+        if flag1:
+            ans = i + 2 - 1
+        
+        if flag2:
+            ans = i + 2
+
+    print(ans)
