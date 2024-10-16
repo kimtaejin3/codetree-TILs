efcfd554b544782531c2d3443c8c6923a049beff
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-10-16 17:54:17 +0900

    Title: 2명의 도둑, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241016/2명의 도둑/two-thieves.py
+++ 241016/2명의 도둑/two-thieves.py
@@ -26,7 +26,28 @@ def remove_fill(row, col):
 
         b[row][i] = 0
 
+new_s = []
+temp_s = []
+max_val = -1
+def get_new_s(s, lev):
+    global new_s, max_val, new_s
+    if lev == len(s):
+        if sum(temp_s) <= c:
+            if max_val < sum(temp_s):
+                max_val = sum(temp_s)
+                new_s = temp_s[:]
+        return
+
+    temp_s.append(s[lev])
+    get_new_s(s, lev + 1)
+
+    temp_s.pop()
+    get_new_s(s, lev + 1)
+
+
 def cal():
+    global new_s, temp_s, max_val
+
     temp = 0
     for i in range(n):
         cnt = 0
@@ -36,15 +57,24 @@ def cal():
                 s.append(a[i][j])
                 cnt += 1
 
-                while sum(s) > c:
-                    s.sort()
-                    s.pop(0)
-            
             if cnt == m:
-                for i in range(len(s)):
-                    temp += s[i] * s[i]
+                # 41 ~ 43이 문제임
+                # while sum(s) > c:
+                #     s.sort()
+                #     s.pop(0)
+                get_new_s(s, 0)
+                
+                # print(new_s)
+
+                # 으아!!!! maxVal 말고 새로운 s 넘어오게....
+                # print(max_val)
+                for i in range(len(new_s)):
+                    temp += new_s[i] * new_s[i]
+                # print(s)
                 cnt = 0
                 s = []
+                new_s, temp_s = [], []
+                max_val = -1
     
     return temp
 
@@ -53,7 +83,9 @@ ans = -1
 def func(lev):
     global ans
     if lev == 2:
+        # print(cal())
         ans = max(ans, cal())
+        # print('==')
         return
 
     for i in range(n):
