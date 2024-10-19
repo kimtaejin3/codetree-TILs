659edf5624032c8322c4493762ebf74443c381c2
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-10-19 14:59:31 +0900

    Title: 1차원 윷놀이, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241019/1차원 윷놀이/yutnori-1d.py
@@ -0,0 +1,36 @@
+n, m, k = map(int, input().split())
+arr = [0] + list(map(int, input().split()))
+choose = []
+# print(arr)
+def calc():
+    # print(choose)
+    a = [0 for _ in range(n+1)]
+    
+    for i in range(len(choose)):
+        a[choose[i]] += arr[i+1]
+    # print(a)
+    cnt = 0
+
+    for el in a:
+        if el >= m:
+            cnt += 1
+    
+        
+    return cnt
+
+ans = 0
+
+def f(lev):
+    global ans
+    if lev == n:
+        ans = max(ans, calc())
+        return
+    
+    for i in range(1, k+1):
+        choose.append(i)
+        f(lev + 1)
+        choose.pop()
+
+
+f(0)
+print(ans)
