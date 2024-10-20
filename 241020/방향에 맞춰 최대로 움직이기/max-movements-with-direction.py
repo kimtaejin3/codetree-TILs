6ee70f5b8af7cb96c870fecfc7e0b69d91ed380c
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-10-20 14:19:40 +0900

    Title: 방향에 맞춰 최대로 움직이기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241020/방향에 맞춰 최대로 움직이기/max-movements-with-direction.py
@@ -0,0 +1,36 @@
+n = int(input())
+
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+grid_d = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+r, c = map(int, input().split())
+r, c = r - 1, c - 1
+
+dxs, dys = [-1, -1, 0, 1, 1, 1, 0, -1],[0, 1, 1, 1, 0, -1, -1, -1]
+
+ans = 0
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def move(x, y, depth):
+
+    nx, ny = x + dxs[grid_d[x][y] - 1], y + dys[grid_d[x][y] - 1]
+    
+    while in_range(nx,ny):
+        if grid[x][y] < grid[nx][ny]:
+            move(nx, ny, depth + 1)
+        
+        x, y = nx, ny
+        nx, ny = x + dxs[grid_d[x][y] - 1], y + dys[grid_d[x][y] - 1]
+
+    print(depth)
+
+move(r, c, 0)
