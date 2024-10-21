18f16e2cdd059ec7707b7f27caa1177016693ebe
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-10-21 11:20:11 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 124ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241021/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
+++ 241021/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -8,9 +8,11 @@ def check():
     s = ''.join(map(str, choose))
 
     for k in range(1, n//2+1):
-        for i in range(n-1):
+        for i in range(len(s)-1):
             if i+k*2 > n:
                 continue
+            print('s:',s)
+            print('temp:',s[i:i+k], s[i+k:i+k*2])
             if s[i:i+k] == s[i+k: i+k*2]:
                 return True
 
@@ -20,20 +22,15 @@ ans = ''
 
 def func(lev):
     global ans
-
+    
     if lev == n:
-        if not check():
-            ans = ''.join(map(str, choose))
-            print(ans)
-
-            #이렇게 하니까 20%까지는 갔음.
-            #다 구해놓고 하는 방식이 아니라 조건에 맞지 않을시 재귀를 하면 안될 것 같음.
-            exit(0)
+        print('choose:',choose)
         return
 
     for i in range(len(nums)):
         choose.append(nums[i])
-        func(lev + 1)
+        if len(choose) == 1 or not check():
+            func(lev + 1)
         choose.pop()
 
 func(0)
