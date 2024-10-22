20d8033b7799ff9dab8b70629e547c1d9e215621
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-10-22 11:36:26 +0900

    Title: 최소 점프 횟수, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241022/최소 점프 횟수/min-num-of-jumps.py
@@ -0,0 +1,35 @@
+import sys
+
+n = int(input())
+a = list(map(int, input().split()))
+
+choose = []
+
+def isPossible():
+
+    for i in range(1, len(choose)):
+        if choose[i-1][0] + choose[i-1][1] < choose[i][1]:
+            return False
+    
+    return True
+
+
+ans = sys.maxsize
+
+def search(lev):
+    global ans
+
+    if lev == n:
+        if choose and choose[0][0] == a[0] and choose[-1][0] == a[-1] and isPossible():
+            ans = min(ans, len(choose)-1)
+        return
+
+    choose.append((a[lev], lev))
+    search(lev + 1)
+    choose.pop()
+
+    search(lev + 1)
+
+search(0)
+
+print(ans)
