4700a30d06d87783f62e357f2c9f3f54b4c54ab2
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-10-23 16:20:58 +0900

    Title: xor 결과 최대 만들기, Time: 287ms, Memory: 28MB, Status: Passed - CodeTree

--- 241023/xor 결과 최대 만들기/max-of-xor.py
+++ 241023/xor 결과 최대 만들기/max-of-xor.py
@@ -11,7 +11,7 @@ def choose(lev, cnt):
         if cnt == m:
             val = 0
             for elem in answer:
-                val = val | elem
+                val = val ^ elem
             ans = max(val, ans)
         return
 
