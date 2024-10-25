7040b806713ac8bff288f6238f826eb8f98bdbb2
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-10-25 09:50:52 +0900

    Title: 그래프 탐색, Time: 172ms, Memory: 26MB, Status: Passed - CodeTree

--- 241025/그래프 탐색/graph-traversal.py
+++ 241025/그래프 탐색/graph-traversal.py
@@ -7,14 +7,16 @@ for _ in range(m):
     x, y = map(int, input().split())
 
     graph[x].append(y)
+    graph[y].append(x)
 
-def dfs(curr_vertex):
-    for vertex in graph[curr_vertex]:
-        if not visited[vertex]:
-            # print(vertex)
-            visited[vertex] = True
-            dfs(vertex)
+def dfs(vertex):
+    for curr_v in graph[vertex]:
+        if not visited[curr_v]:
+            # print(curr_v)
+            visited[curr_v] = True
+            dfs(curr_v)
 
 # print(graph)
+visited[1] = True
 dfs(1)
-print(sum([1 for elem in visited if elem == True]))
+print(sum([1 for elem in visited[2:] if elem == True]))
