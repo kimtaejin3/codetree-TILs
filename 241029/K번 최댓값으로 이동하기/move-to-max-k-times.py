c8090a5fc4d46473ef5e970858951fce5ea9aa13
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-10-29 20:10:54 +0900

    Title: K번 최댓값으로 이동하기, Time: 439ms, Memory: 29MB, Status: Passed - CodeTree

--- 241029/K번 최댓값으로 이동하기/move-to-max-k-times.py
+++ 241029/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -1,70 +1,91 @@
 from collections import deque
 
-n, k = map(int, input().split())
+NOT_EXISTS = (-1, -1)
+
+n, k = tuple(map(int, input().split()))
 grid = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
+
+r, c = tuple(map(int, input().split()))
+curr_cell = (r - 1, c - 1)
+
+q = deque()
 visited = [
-    [False for _ in range(n)]
+    [0 for _ in range(n)]
     for _ in range(n)
 ]
-r, c = map(int, input().split())
-r, c = r - 1, c - 1
 
 def in_range(x, y):
-    return 0 <= x < n and 0 <= y < n
+    return 0 <= x and x < n and 0 <= y < n
 
-def find_max_pos(pos):
+def can_go(x, y, target_num):
+    return in_range(x, y) and not visited[x][y] and \
+           grid[x][y] < target_num
 
-    q = deque([pos])
-    curr_num = grid[pos[0]][pos[1]]
-    max_poses = []
-    moves = []
-    max_num = -1
-    dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
-    
-    while q:
-        x, y = q.popleft()
-
-        for dx, dy in zip(dxs, dys):
-            nx, ny = x + dx, y + dy
-            
-            if in_range(nx, ny) and grid[nx][ny] < curr_num and not visited[nx][ny]:
-                visited[nx][ny] = True
-                # print(grid[nx][ny], end=' ')
-                moves.append((grid[nx][ny], (nx, ny)))
-                if max_num <= grid[nx][ny]:
-                    max_num = grid[nx][ny]
-                    # max_poses.append((nx, ny))
-
-                q.append((nx, ny))
-
-    for m in moves:
-        num, pos = m
-        if num == max_num:
-            max_poses.append(pos)
-
-    if len(max_poses) == 0:
-        return (-1, -1)
-    else:
-        return sorted(max_poses)[0]
-
-
-pre_r, pre_c = r, c
-ans_r, ans_c = -1, -1
-
-for _ in range(k):
-    new_r, new_c = find_max_pos((pre_r, pre_c))
+def initialize_visited():
     for i in range(n):
         for j in range(n):
             visited[i][j] = False
 
-    if new_r == -1 and new_c == -1:
-        ans_r, ans_c = pre_r, pre_c
-        break
-    else:
-        pre_r, pre_c = new_r, new_c
-        ans_r, ans_c = new_r, new_c
+def bfs():
+    dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
 
-print(ans_r + 1, ans_c + 1)
+    curr_x, curr_y = curr_cell
+    visited[curr_x][curr_y] = True
+    q.append(curr_cell)
+
+    target_num = grid[curr_x][curr_y]
+
+    while q:
+        curr_x, curr_y = q.popleft()
+
+        for dx, dy in zip(dxs, dys):
+            new_x, new_y = curr_x + dx, curr_y + dy
+
+            if can_go(new_x, new_y, target_num):
+                q.append((new_x, new_y))
+                visited[new_x][new_y] = True
+
+def need_update(best_pos, new_pos):
+    if best_pos == NOT_EXISTS:
+        return True
+    
+    best_x, best_y = best_pos
+    new_x, new_y = new_pos
+
+    return (grid[new_x][new_y], -new_x, -new_y) > \
+           (grid[best_x][best_y], -best_x, -best_y)
+
+def move():
+    global curr_cell
+
+    initialize_visited()
+
+    bfs()
+
+    best_pos = NOT_EXISTS
+    for i in range(n):
+        for j in range(n):
+            if not visited[i][j] or (i, j) == curr_cell:
+                continue
+            
+            new_pos = (i, j)
+            if need_update(best_pos, new_pos):
+                best_pos = new_pos
+    
+    if best_pos == NOT_EXISTS:
+        return False
+    else:
+        curr_cell = best_pos
+        return True
+
+for _ in range(k):
+    is_moved = move()
+
+    if not is_moved:
+        break
+
+final_x, final_y = curr_cell
+print(final_x + 1, final_y + 1)
