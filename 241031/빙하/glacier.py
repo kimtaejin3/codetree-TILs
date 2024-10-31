e7c195b69c03da3d406df61c7e31ba5e00c6f649
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-10-31 11:13:43 +0900

    Title: 빙하, Time: 1287ms, Memory: 30MB, Status: Passed - CodeTree

--- 241031/빙하/glacier.py
+++ 241031/빙하/glacier.py
@@ -1,82 +1,98 @@
 from collections import deque
+import enum
+
+class Element(enum.Enum):
+    WATER = 0
+    GLACIER = 1
 
 n, m = tuple(map(int, input().split()))
 
-grid = [
+a = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
 
+q = deque()
+glaciers_to_melt = deque()
+visited = [
+    [False for _ in range(m)]
+    for _ in range(n)
+]
+cnt = 0
+
+dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+
+elapsed_time = 0
+last_melt_cnt = 0
+
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < m
 
-def bfs(x, y):
-    q = deque([(x, y)])
-    dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
-    grid[x][y] = 2
+def can_go(x, y):
+    return in_range(x, y) and a[x][y] == Element.WATER.value and not visited[x][y]
+
+def is_glacier(x, y):
+    return in_range(x, y) and a[x][y] == Element.GLACIER.value and not visited[x][y]
+
+def initialize():
+    for i in range(n):
+        for j in range(m):
+            visited[i][j] = False
+
+def bfs():
+    initialize()
+
+    q.append((0, 0))
+    visited[0][0] = True
 
     while q:
         x, y = q.popleft()
 
         for dx, dy in zip(dxs, dys):
-            nx, ny = x + dx, y + dy
-            if in_range(nx, ny) and grid[nx][ny] == 0:
-                grid[nx][ny] = 2
-                q.append((nx, ny))
+            new_x, new_y = x + dx, y + dy
+
+            if can_go(new_x, new_y):
+                q.append((new_x, new_y))
+                visited[new_x][new_y] = True
+
+def outside_water_exist_in_neighbor(x, y):
+    for dx, dy in zip(dxs, dys):
+        new_x, new_y = x + dx, y + dy
+        if in_range(new_x, new_y) and visited[new_x][new_y]:
+            return True
+    
+    return False
 
 def melt():
-    dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
-
-    bfs(0, 0)
+    global last_melt_cnt
 
     for i in range(n):
         for j in range(m):
-            if grid[i][j] == 1:
-                for dx, dy in zip(dxs, dys):
-                    nx, ny = i + dx, j + dy
+            if a[i][j] == Element.GLACIER.value and \
+                outside_water_exist_in_neighbor(i, j):
+                a[i][j] = Element.WATER.value
+                last_melt_cnt += 1
 
-                    if in_range(nx, ny) and grid[nx][ny] == 2:
-                        grid[i][j] = 0
-    
-    for i in range(n):
-        for j in range(m):
-            if grid[i][j] == 2:
-                grid[i][j] = 0
-    
-   
+def simulate():
+    global elapsed_time, last_melt_cnt
 
-ans = 0
-t = 0
-for i in range(n):
-    for j in range(m):
-        if grid[i][j] == 1:
-            ans += 1
+    elapsed_time += 1
+    last_melt_cnt = 0
 
-def melt_all():
-    global ans, t
-
-    cnt = 0
-    t += 1
+    bfs()
     melt()
 
-    for row in grid:
-        for elem in row:
-            if elem == 1:
-                cnt += 1
+def glacier_exist():
+    for i in range(n):
+        for j in range(m):
+            if a[i][j] == Element.GLACIER.value:
+                return True
     
-    if cnt > 0:
-        ans = cnt
-    
-    if cnt > 0:
-        melt_all()
+    return False
 
-melt_all()
+while True:
+    simulate()
+    if not glacier_exist():
+        break
 
-# for row in grid:
-#     for elem in row:
-#         print(elem, end=' ')
-#     print()
-
-# print(t)
-# print(ans)
-print(t, ans)
+print(elapsed_time, last_melt_cnt)
