3cb8fbfba078a36db0a50fb8e698c9f36a9696ce
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-11-07 17:02:58 +0900

    Title: 서로 다른 BST 개수 세기, Time: 61ms, Memory: 15MB, Status: Passed - CodeTree

--- 241107/서로 다른 BST 개수 세기/number-of-unique-bst.py
+++ 241107/서로 다른 BST 개수 세기/number-of-unique-bst.py
@@ -11,8 +11,8 @@ dp[1] = 1
 dp[2] = 2
 
 for i in range(3, n + 1):
-    dp[i] = dp[i-1]
-    for j in range(1, i):
+    # dp[i] = dp[i-1]
+    for j in range(i):
         dp[i] +=  dp[j] * dp[i - j - 1]
     
 print(dp[n])
