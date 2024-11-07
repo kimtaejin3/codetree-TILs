d7b48a0cd91554ce7ab9f18cf4eb0311f48ddd30
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-11-07 10:11:52 +0900

    Title: 계단 오르기, Time: 98ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241107/계단 오르기/climbing-stairs.py
+++ 241107/계단 오르기/climbing-stairs.py
@@ -6,6 +6,9 @@ def climb(N):
     if N == 2 or N == 3:
         return 1
     
+    if memo[N] != -1:
+        return memo[N]
+    
     memo[N] = (climb(N - 2) + climb(N - 3)) % 10007
 
     return memo[N]
