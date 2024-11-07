708020972f3800ce2ddd0f9441a0c37395a2311e
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-11-07 11:22:10 +0900

    Title: 사각형 채우기 3, Time: 52ms, Memory: 16MB, Status: Passed - CodeTree

--- 241107/사각형 채우기 3/rectangle-fill-3.py
+++ 241107/사각형 채우기 3/rectangle-fill-3.py
@@ -1,12 +1,15 @@
+MOD = 1000000007
 N = 1001
 
 dp = [0] * N
 
+dp[0] = 1
 dp[1] = 2
-dp[2] = 7
 
-for i in range(3, N):
-    dp[i] = (dp[i-1] * 2 + dp[i-2] * 4 - 1) % 1000000007
+for i in range(2, N):
+    dp[i] = (dp[i-1] * 2 + dp[i-2] * 3) % MOD
+    for j in range(i-3, -1, -1):
+        dp[i] =  (dp[i] + dp[j] * 2) % MOD
 
 n = int(input())
 print(dp[n])
