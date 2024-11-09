206a377f465fa6e7e97148f5a8c2099ef92bd75d
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-11-09 17:33:01 +0900

    Title: 정수 사각형 최대 합, Time: 61ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 241109/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+a = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+dp = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+dp[0][0] = a[0][0]
+
+for i in range(1, n):
+    dp[0][i] = dp[0][i - 1] + a[0][i] 
+
+for i in range(1, n):
+    dp[i][0] = dp[i - 1][0] + a[i][0]
+
+for i in range(1, n):
+    for j in range(1, n):
+        dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]) + a[i][j]
+
+print(dp[n-1][n-1])
