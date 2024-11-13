bae98281b80ce0d8c0c08cad802507974d2074a1
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-11-14 07:52:50 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 62ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241114/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -0,0 +1,22 @@
+#그리디
+n = int(input())
+
+c = []
+for _ in range(n):
+    c.append(tuple(map(int, input().split())))
+
+c.sort(key = lambda x : (x[1], x[0]))
+
+cnt = 1
+
+s, e = c[0]
+for i in range(1, len(c)):
+    s_t, e_t = c[i]
+
+    if s_t >= e:
+        s = s_t
+        e = e_t
+        cnt += 1
+
+print(cnt)
+
