f9396a680d8b2e7f3a7c1c37f2623cc4ede0ca89
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-11-15 13:52:26 +0900

    Title: 증가했다가 감소하는 부분 수열, Time: 57ms, Memory: 17MB, Status: Passed - CodeTree

--- 241115/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.py
+++ 241115/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.py
@@ -1,20 +1,29 @@
 n = int(input())
-num = list(input().split())
 
-dp = [
-    [1 for _ in range(2)]
-    for _ in range(n)
-]
+arr = list(map(int, input().split()))
+
+dp = [[0, 0] for _ in range(n)]
+
+dp[0][0] = 1
+dp[0][1] = 1
 
 for i in range(1, n):
+    max_val = 0
+    max_val2 = 0
+
     for j in range(i):
-        if num[i] > num[j]:
-            dp[i][0] = max(dp[i][0], dp[j][0] + 1)
-        elif num[i] < num[j]:
-            dp[i][1] = max(dp[i][1], dp[j][1] + 1, dp[j][0] + 1)
+        if arr[j] < arr[i]:
+            max_val = max(max_val, dp[j][0])
+        elif arr[j] > arr[i]:
+            max_val2 = max(max_val2, dp[j][1], dp[j][0])
 
-ans = -1
+    dp[i][0] = max_val + 1
+    dp[i][1] = max_val2 + 1
+
+max_val = -1
+
 for i in range(n):
-    ans = max(ans, dp[i][0], dp[i][1])
+    a, b = dp[i]
+    max_val = max(max_val, a, b)
 
-print(ans)
+print(max_val)
